835f6c2901124d1ea85d8da7e7f0e615ed65923c
gittjar <jarno.keranen@gmail.com>
2023-02-14 12:49:27 +0200

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NumbersComponent } from './numbers/numbers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NumbersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/numbers/numbers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-numbers',
+  templateUrl: './numbers.component.html',
+  styleUrls: ['./numbers.component.css']
+})
+export class NumbersComponent {
+
+}
